16719918de0cc30cc16cebfcf6a7ebaf3905aab8
alitamtam <alitamtam@oclock.school>
2024-09-19 18:17:28 +0200

    feat: rendering profile for publication

--- frontend/client/src/components/Profiles/ProfileDetails.jsx
+++ frontend/client/src/components/Profiles/ProfileDetails.jsx
@@ -21,7 +21,7 @@ const ProfileDetails = () => {
                         alt={publication.owner.first_name}
                         className="w-48 h-48 object-cover rounded-full mb-4"
                     />
-                    <h2 className="text-2xl font-bold text-gray-800">
+                    <h2 className="text-2xl font-bold text-gray-800 capitalize">
                         {publication.owner.first_name} {publication.owner.last_name}
                     </h2>
                 </div>
