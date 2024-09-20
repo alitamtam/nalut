169e2cc6e313507b21d7a75ac731d5e850eb61f0
alitamtam <alitamtam@oclock.school>
2024-09-20 18:39:05 +0200

    style: profiles cards

--- frontend/client/src/components/About/MemberCard.jsx
+++ frontend/client/src/components/About/MemberCard.jsx
@@ -2,23 +2,23 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 
 const MemberCard = ({ member }) => (
-    console.log("members list", member),
-    <div className="bg-white shadow-md p-4 rounded-md text-center">
+    <div className="bg-blue-950 shadow-md p-4 text-white rounded-none text-center capitalize">
         <img
             src={`data:image/jpeg;base64,${member.image}`}// Handle missing photo
             alt={member.fullName || 'Member Photo'}             // Use fullName for alt attribute
             className="w-32 h-32 mx-auto rounded-full object-cover"
         />
         <h3 className="mt-4 font-bold">{member.fullName || 'Name not available'}</h3> {/* Use fullName */}
-        <p className="text-sm text-gray-500">{member.title || 'Title not available'}</p>
+        <p className="text-sm text-white">{member.title || 'Title not available'}</p>
 
         {/* Use Link for navigation to the profile details page */}
-        <Link
+        <div className='my-10  '> <Link
             to={`/profileDisplay/${member.id}`} // Link to the profile details page using member's id
-            className="mt-4 bg-teal-500 text-white py-2 px-4 rounded-md inline-block"
+            className="  text-white border-2 border-white rounded-full hover:bg-teal-500 bg-blue-950 hover:text-white py-2 px-8 font-sans hover:border-none"
         >
             View Details
-        </Link>
+        </Link></div>
+
     </div>
 );
 
