8b812c18432e659dfdbe31e1c2c61d1d6732c79e
alitamtam <alitamtam@oclock.school>
2024-09-23 17:52:58 +0200

    fix : profile display api

--- frontend/client/src/components/Admin/Dashboard/hooks/useGetProfileById.js
+++ frontend/client/src/components/Admin/Dashboard/hooks/useGetProfileById.js
@@ -1,14 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
 import api from "../../../../api/axiosConfig";
 
-export const useGetProfileById = (userId) => {
+export const useGetProfileById = (id) => {
   return useQuery({
-    queryKey: ["profile", userId],
+    queryKey: ["profile", id],
     queryFn: async () => {
-      const response = await api.get(`/api/profile/${userId}`);
+      const response = await api.get(`/api/profiles/${id}`);
 
       return response.data;
     },
-    enabled: !!userId, // This makes sure the query only runs if userId is defined
+    enabled: !!id, // This makes sure the query only runs if userId is defined
   });
 };
