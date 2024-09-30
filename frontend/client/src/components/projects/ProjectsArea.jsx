09dc9f90c964317f41b77d99e11e7bedd6f665cd
alitamtam <alitamtam@oclock.school>
2024-09-30 13:28:58 +0200

    style: projects area

--- frontend/client/src/components/projects/ProjectsArea.jsx
+++ frontend/client/src/components/projects/ProjectsArea.jsx
@@ -43,7 +43,7 @@ const ProjectsArea = () => {
     const lastThreePublications = projects.slice(-9);
 
     return (
-        <div className="p-4">
+        <div className="p-4 ">
             <div className="flex flex-col items-center lg:flex-row lg:justify-between lg:mx-80 pb-4">
                 <h2 className="text-3xl font-bold lg:mb-4 text-center capitalize font-body text-gray-800">
                     Projects Area
@@ -54,35 +54,43 @@ const ProjectsArea = () => {
             </div>
             <div className=" bg-white text-gray-800 lg:h-4/5 border-b border-teal-600 pb-12 mb-12 w-full max-w-full overflow-x-hidden">
 
-                <div className=" lg:mx-80  bg-white px-3 space-y-12">
+                <div className=" lg:mx-80  bg-white w-auto  ">
 
-                    <Slider {...settings} >
+                    <Slider {...settings}>
                         {lastThreePublications.map((project) => (
-                            <div key={project.id} className="  bg-neutral-200  m-4  p-4  space-y-4 py-12">
-                                <Link to={`/projects/${project.id}`} className=" space-y-12 text-xl font-semibold capitalize hover:text-teal-600">{project.title}</Link>
-                                {project.project_image && (
-                                    <Link to={`/projects/${project.id}`} >
-                                        <img
-                                            src={project.project_image}
-                                            alt={project.title}
-                                            className="w-full lg:h-60 object-cover  mt-2 my-4"
-                                        /></Link>
-                                )}
-                                <Link to={`/profileDisplay/${project.actors.profile?.id}`}>
-                                    <span className="text-gray-600">By |</span>  <span className="text-orange-600 hover:underline"> {project.actors.first_name} {project.actors.last_name}</span>
-                                </Link>
-
+                            <div key={project.id} className=" py-12 px-1 ">
+                                <div className="bg-neutral-100 p-6">
+                                    <div >
+                                        <Link to={`/projects/${project.id}`} className="lg:text-ssm font-semibold capitalize hover:text-teal-600 py-6">
+                                            <p className="h-12 mb-6">{project.title}</p>
+                                        </Link>
+                                    </div>
+                                    {project.project_image && (
+                                        <Link to={`/projects/${project.id}`}>
+                                            <div>
+                                                <img
+                                                    src={project.project_image}
+                                                    alt={project.title}
+                                                    className="w-[480px] lg:h-60 object-cover mt-2 my-4"
+                                                />
+                                            </div>
+                                        </Link>
+                                    )}
+                                    <Link to={`/profileDisplay/${project.actors.profile?.id}`}>
+                                        <span className="text-gray-600">By |</span>
+                                        <span className="text-orange-600 hover:underline"> {project.actors.first_name} {project.actors.last_name}</span>
+                                    </Link></div>
                             </div>
                         ))}
                     </Slider>
                 </div>
-                <div className="pt-10 ">
+                <div className="pt-10 flex justify-center ">
                     <Link to='/projects/ViewAllProjects' className="border-2 text-teal-600 font-body border-teal-600 rounded-full hover:bg-teal-600 hover:text-white py-2 px-14 mb-4 lg:mb-0 lg:hidden md:block ssm:block ">
                         View All
                     </Link>
                 </div>
-            </div>
-        </div>
+            </div >
+        </div >
     );
 };
 
