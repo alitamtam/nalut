4cfe8c36eb84480d98f63c21ce6f9787153a9b38
alitamtam <alitamtam@oclock.school>
2024-10-02 00:07:55 +0200

    feat fix topics icons

--- frontend/client/src/components/TopicSlick.jsx
+++ frontend/client/src/components/TopicSlick.jsx
@@ -2,15 +2,33 @@
 import PropTypes from 'prop-types';
 import Slider from "react-slick";
 import { useGetTopics } from "./Admin/Dashboard/hooks/useGetTopics"; // Use hook to fetch topics
-import { FaBook } from 'react-icons/fa'; // Import the icon component
-import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 import { Link, useNavigate } from 'react-router-dom'; // Import useNavigate
 import { useTranslation } from 'react-i18next'; // Import the hook
-import { useIconOptions } from './Admin/Dashboard/hooks/useIconOptions'; // Import the mapping of icon names to components
+import { FaUserGraduate, FaBook, FaLaptop, FaBrain, FaLeaf, FaPenAlt } from 'react-icons/fa';
+import { LiaChalkboardTeacherSolid } from 'react-icons/lia';
+import { FaSchoolCircleCheck } from 'react-icons/fa6';
+import { BsBuildingAdd } from 'react-icons/bs';
+import { RiGovernmentLine } from 'react-icons/ri';
+import { AiOutlineFileProtect } from 'react-icons/ai';
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
+// import { useIconOptions } from './Admin/Dashboard/hooks/useIconOptions'; // Import the mapping of icon names to components
 
 // Define a mapping of icon names to components
 
-
+// Define a mapping of icon names to components
+const iconMap = {
+    'Teacher Education': <LiaChalkboardTeacherSolid className="text-6xl" />,
+    'Teacher Professional Development': <FaUserGraduate className="text-6xl" />,
+    'School Governance': <FaSchoolCircleCheck className="text-6xl" />,
+    'Ministry of Education': <RiGovernmentLine className="text-6xl" />,
+    'Buildings and Facilities': <BsBuildingAdd className="text-6xl" />,
+    'Quality Assurance': <AiOutlineFileProtect className="text-6xl" />,
+    'Educational Research': <FaBook className="text-6xl" />,
+    'Information Technology in Schools': <FaLaptop className="text-6xl" />,
+    'Inclusion and Neuro-divergence': <FaBrain className="text-6xl" />,
+    'Student Wellbeing and Enrichment': <FaLeaf className="text-6xl" />,
+    'Assessment and Examination': <FaPenAlt className="text-6xl" />,
+};
 // Custom next and previous arrows
 const NextArrow = ({ onClick }) => (
     <button
@@ -39,7 +57,7 @@ const TopicsSlick = () => {
     // Fetch the topics using your custom hook
     const { data: topics, isLoading, error } = useGetTopics();
     const { t, i18n } = useTranslation('navbar'); // Use the hook to get the translation function
-    const iconOptions = useIconOptions(); // Get the icon options
+    // const iconOptions = useIconOptions(); // Get the icon options
     const isArabic = i18n.language === 'ar'; // Check if the current language is Arabic
     const navigate = useNavigate(); // Initialize useNavigate
 
@@ -92,9 +110,9 @@ const TopicsSlick = () => {
                             >
                                 {/* Content goes here */}
 
-                                <div className="lg:text-6xl text-teal-600 group-hover:text-white pb-4 pl-20 lg:pt-10 ssm:pt-8 ">
+                                <div className="lg:text-6xl text-teal-600 group-hover:text-white pb-4 pl-20  lg:pt-10 ssm:pt-8 lg:border lg:rounded-full ">
                                     {/* Dynamically render the icon based on iconClass */}
-                                    {iconOptions[topic.iconClass] || <FaBook className="text-6xl" />}
+                                    {iconMap[topic.iconClass] || <FaBook className="text-6xl" />}
                                 </div>
                                 <div className="flex-col">
                                     <p className={`${isArabic ? ' lg:text-base lg:font-arabic ssm:font-arabic md:font-arabic ssm:text-sm ssm:font-bold' : ''} lg:mt-4 text-teal-700  ssm:text-xs lg:text-sm font-body tracking-wider uppercase text-center group-hover:text-white pb-10`}>
