cc7f9163872b83d57f655ec9936900e4604b5592
alitamtam <alitamtam@oclock.school>
2024-10-05 16:37:26 +0200

    prisma config

--- backend/prisma/index.js
+++ backend/prisma/index.js
@@ -1,5 +1,14 @@
-import { PrismaClient } from "@prisma/client";
+import pkg from "pg"; // Import the CommonJS module using default import
+const { Pool } = pkg; // Destructure Pool from the default export
 
-const prisma = new PrismaClient();
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
