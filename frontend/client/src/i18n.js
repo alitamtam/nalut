386a273ef99e04e3fb5eb967552b61f3da2553a3
alitamtam <alitamtam@oclock.school>
2024-10-07 23:47:33 +0200

    fix error

--- frontend/client/src/i18n.js
+++ frontend/client/src/i18n.js
@@ -1,30 +1,21 @@
+// src/i18n.js
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import Backend from "i18next-http-backend";
-
-const isDevelopment = import.meta.env.MODE === "development"; // Check if it's in development mode
+import Backend from "i18next-http-backend"; // For loading translations from the server
 
 i18n
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: "en",
-    supportedLngs: ["en", "ar"], // Add supported languages
-
-    debug: isDevelopment, // Enable debug in development only
+    fallbackLng: "en", // Default language
+    debug: true, // Set to false in production
     interpolation: {
-      escapeValue: false,
+      escapeValue: false, // React already escapes values
     },
     backend: {
-      // Dynamically switch the loadPath based on the environment
-      loadPath: isDevelopment
-        ? "http://www.localhost:3000/locales/{{lng}}/{{ns}}.json" // Local path for development
-        : "https://edulibya.ly/locales/{{lng}}/{{ns}}.json", // Full URL for production
-    },
-    cache: {
-      enabled: !isDevelopment, // Disable cache in development, enable in production
+      loadPath: `${import.meta.env.VITE_API_URL}/locales/{{lng}}/{{ns}}.json`, // Translation file path
     },
   });
 
