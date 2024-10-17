b9b6b7fa9e3d7334e58542a10e09e2e68eee5c2d
alitamtam <alitamtam@oclock.school>
2024-10-17 12:44:01 +0200

    add: SSL certificates

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,22 @@
 import { createServer } from "node:http";
+import { readFileSync } from "fs"; // Import fs module to read files
 import "dotenv/config";
 import app from "./app.js";
 
+// Load SSL certificate files
+const sslOptions = {
+  key: readFileSync("./src/ssl/private.key"), // Path to your private key file
+  cert: readFileSync("./src/ssl/certificate.crt"), // Path to your certificate file
+  // Uncomment if you have a CA bundle
+  // ca: readFileSync('./src/ssl/ca_bundle.crt'), // Path to your CA bundle file (if applicable)
+};
+
 const PORT = process.env.PORT || 3000;
 
-const httpServer = createServer(app);
+// Create HTTPS server
+const httpsServer = createServer(sslOptions, app);
 
-httpServer.listen(PORT, "0.0.0.0", () => {
+httpsServer.listen(PORT, "0.0.0.0", () => {
   if (process.env.NODE_ENV !== "production") {
     console.log(`🚀 HTTP Server launched at http://localhost:${PORT} 🚀`);
   } else {
