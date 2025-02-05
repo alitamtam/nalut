50ffe672924d54894d7f06eebdbbfb39c3a8889f
alitamtam <alitamtam@oclock.school>
2025-02-05 21:37:52 +0100

    feat: navbar

--- nalut-city-frontend/src/app/shared/components/navbar/navbar.component.ts
+++ nalut-city-frontend/src/app/shared/components/navbar/navbar.component.ts
@@ -1,8 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit ,Inject,PLATFORM_ID} from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { isPlatformBrowser } from '@angular/common';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import {MatToolbar} from '@angular/material/toolbar';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
-})
-export class NavbarComponent {}
+  imports: [
+    CommonModule,
+    MatSlideToggleModule,
+     MatToolbarModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatToolbar,
+    MatIconModule],
+    selector: 'navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.scss'],
+  })
+  export class NavbarComponent implements OnInit {
+    isMobile = false;
+  
+    constructor(@Inject(PLATFORM_ID) private platformId: object) {}
+  
+    ngOnInit() {
+      this.checkScreenSize();
+    }
+  
+    @HostListener('window:resize', [])
+    checkScreenSize() {
+      if (isPlatformBrowser(this.platformId)) {
+        // ✅ Fix for window undefined error in SSR
+        this.isMobile = window.innerWidth < 768;
+      }
+    }
+  }
